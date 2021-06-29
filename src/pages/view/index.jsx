8dc2ae3f9d1e89f01165182fcc869e9fd58f1a67
zhenglipeng <zhenglipeng@sensetime.com>
2021-06-29 18:50:56 +0800

    框架搭建

--- src/pages/view/index.jsx
+++ src/pages/view/index.jsx
@@ -5,6 +5,7 @@ import 'swiper/swiper-bundle.css';
 
 import PageContainer from '@/components/page-container';
 import ElementContainer from '@/components/element-container';
+import Audio from '@/components/audio';
 import elements from '@/components/elements';
 
 import data from './data.json';
@@ -12,36 +13,39 @@ import data from './data.json';
 // direction:   'horizontal' | 'vertical'
 // effect:      'slide' | 'fade' | 'cube' | 'coverflow' | 'flip'
 export default () => {
-  const dataSource = data.map((d, i) => ({
+  const dataSource = data.pages.map((d, i) => ({
     ...d,
     id: i
   }));
 
   return (
-    <Swiper
-      direction="vertical"
-      effect="slide"
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      {dataSource.map(page => (
-        <SwiperSlide key={page.id}>
-          <PageContainer background={page.background}>
-            {page.components && page.components.map((d, i) => {
-              const C = elements[d.type];
-              return (
-                <ElementContainer
-                  key={i} 
-                  position={d.position}
-                  rotation={d.rotation}
-                >
-                  <C src={d.src} />
-                </ElementContainer>
-              );
-            })}
-          </PageContainer>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      <Audio {...data.audio} />
+      <Swiper
+        direction="vertical"
+        effect="slide"
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {dataSource.map(page => (
+          <SwiperSlide key={page.id}>
+            <PageContainer background={page.background}>
+              {page.components && page.components.map((d, i) => {
+                const C = elements[d.type];
+                return (
+                  <ElementContainer
+                    key={i} 
+                    position={d.position}
+                    rotation={d.rotation}
+                  >
+                    <C src={d.src} />
+                  </ElementContainer>
+                );
+              })}
+            </PageContainer>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   )
 }
