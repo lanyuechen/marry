dc0f66b7c93a2efd557d0de6a4ad66d5f5f68af7
zhenglipeng <zhenglipeng@sensetime.com>
2021-08-13 17:55:45 +0800

    详情页优化

---
+++ src/components/page-header/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { history } from 'umi';
+import { PageHeader } from 'antd';
+
+import style from './style.less';
+
+export default (props) => {
+
+  const handleBack = () => {
+    history.goBack();
+  }
+
+  return (
+    <PageHeader
+      className={style.header}
+      onBack={handleBack}
+      {...props}
+    />
+  );
+}
--- src/pages/templates/detail/index.jsx
+++ src/pages/templates/detail/index.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { history } from 'umi';
-import { Typography } from 'antd';
+import { Typography, Space, Button } from 'antd';
+import Icon from '@/components/icon';
 import Cover from '@/components/cover';
+import PageHeader from '@/components/page-header';
 import { fetchData } from '@/utils/utils';
+import * as storyService from '@/services/story';
 
 import style from './style.less';
 
@@ -20,8 +23,20 @@ export default (props) => {
     return null;
   }
 
+  const handlePreview = () => {
+    history.push(`/templates/${id}/view`);
+  }
+
+  const handleEdit = async () => {
+    const res = await storyService.insert(data);
+    if (res.success) {
+      history.push(`/editor/${res.data._id}`);
+    }
+  }
+
   return (
     <div>
+      <PageHeader title={data.name} />
       <div className={style.banner}>
         <img className={style.bg} src={data.cover} />
         <div className={style.content}>
@@ -29,10 +44,18 @@ export default (props) => {
             <Cover cover={data.cover} />
           </div>
           <div className={style.info}>
-            <Typography.Title level={4}>{data.name}</Typography.Title>
-            <Typography.Paragraph ellipsis={{ rows: 5 }}>
+            <Typography.Title level={5}>{data.name}</Typography.Title>
+            <Typography.Paragraph ellipsis={{ rows: 4 }}>
               {data.description}
             </Typography.Paragraph>
+            <Space style={{float: 'right'}}>
+              <Button shape="circle" onClick={handlePreview}>
+                <Icon type="icon-view" />
+              </Button>
+              <Button shape="circle" onClick={handleEdit}>
+                <Icon type="icon-edit" />
+              </Button>
+            </Space>
           </div>
         </div>
       </div>
