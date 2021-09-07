299bfe6e49a3fdc5cffbaf04c705ffb9eae91a62
zhenglipeng <zhenglipeng@sensetime.com>
2021-09-07 18:08:16 +0800

    静态文件通过cdn访问

--- src/components/loading/index.jsx
+++ src/components/loading/index.jsx
@@ -6,7 +6,7 @@ export default () => {
   return (
     <div className={style.loading}>
       <lottie-player
-        src="./loading.json"
+        src="https://cdn.jsdelivr.net/gh/lanyuechen/marry@gh-pages/loading.json"
         background="transparent"
         speed="1"
         style={{width: 100, height: 100}}
