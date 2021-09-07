d3c72322b5cb415d7e85ac5e9af0bf5bdf507d0e
zhenglipeng <zhenglipeng@sensetime.com>
2021-09-07 13:12:36 +0800

    封面修改

--- src/components/cover/index.jsx
+++ src/components/cover/index.jsx
@@ -3,16 +3,20 @@ import { Image } from 'antd';
 import AspectCard from '@/components/aspect-card';
 import style from './style.less';
 
-const LINE = 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="1080" height="1490"><rect fill-opacity="0"/></svg>'
-
 export default (props) => {
   const { cover, width, onClick } = props;
   return (
     <AspectCard aspect={1.414} width={width}>
       <div className={style.book} onClick={onClick}>
         <div className={style.paper} />
-        <img src={LINE} style={{backgroundImage: `url(${cover})`}} />
-        <img src={LINE} className={style.corner} style={{backgroundImage: `url(${cover})`}} />
+        <img src={cover} className={style.corner} />
+        <Image
+          width="100%"
+          height="100%"
+          src={cover}
+          className={style.cover}
+          preview={false}
+        />
       </div>
     </AspectCard>
   )
