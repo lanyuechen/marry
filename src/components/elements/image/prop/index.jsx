7c67b0cea69ee596fb98b7bcb3f72dd143dea393
zhenglipeng <zhenglipeng@sensetime.com>
2021-09-15 18:41:27 +0800

    图片属性编辑修改

--- src/components/elements/image/prop/index.jsx
+++ src/components/elements/image/prop/index.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import { Card, Form, Upload, Button } from 'antd';
 import ImageCrop from './image-crop';
 
 export default (props) => {
   const { element, onChange } = props;
 
   return (
-    <ImageCrop
-      src={element.props.src}
-      aspect={element.size[0] / element.size[1]}
-      crop={element.props.crop}
-      onChange={crop => onChange('props.crop', crop)}
-    />
-  )
+    <Card>
+      <Form>
+        <Form.Item label="上传图片">
+          <Upload>
+            <Button>上传图片</Button>
+          </Upload>
+        </Form.Item>
+        <Form.Item label="图片调整">
+          <ImageCrop
+            src={element.props.src}
+            aspect={element.size[0] / element.size[1]}
+            crop={element.props.crop}
+            onChange={crop => onChange('props.crop', crop)}
+          />
+        </Form.Item>
+      </Form>
+    </Card>
+  );
 }
